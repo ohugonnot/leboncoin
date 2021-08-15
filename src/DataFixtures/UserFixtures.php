303401cefacc022d27e7724e5d44c89cff61b64a
asfolken <folken70@hotmail.com>
2021-08-15 19:27:33 +0200

    Maj des fixtures

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -5,23 +5,32 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Generator;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixtures extends Fixture
 {
     private UserPasswordHasherInterface $passwordHasher;
+    private Generator $faker;
 
     public function __construct(UserPasswordHasherInterface $passwordHasher)
     {
         $this->passwordHasher = $passwordHasher;
+        $this->faker = Factory::create();
     }
 
     public function load(ObjectManager $manager)
     {
         foreach (range(0, 12) as $number) {
             $user = new User();
-            $user->setEmail("test{$number}@test.test");
+            $email = "test{$number}@test.test";
+            $user->setEmail($email);
+            $user->setRoles(['ROLE_USER']);
             $user->setPassword($this->passwordHasher->hashPassword($user,'test'));
+            $user->setFirstname($this->faker->firstName);
+            $user->setLastname($this->faker->lastName);
+            $user->setApiToken($email);
             $this->addReference($number, $user);
             $manager->persist($user);
         }
